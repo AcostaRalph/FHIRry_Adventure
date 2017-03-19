e49cb9367c1ba5d1342a5cb3f2621a5cc2728bbe
Jason <xdragonx57@gmail.com>
2017-03-18 19:58:35 -0700

    Broken SHA256 Code

--- app/src/main/java/com/example/kamuela94/fhirry_adventure/LoginActivity.java
+++ app/src/main/java/com/example/kamuela94/fhirry_adventure/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.kamuela94.fhirry_adventure;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +21,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,6 +32,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,6 +68,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        PINCheck pinCheck = new PINCheck("1234", this);
+        try {
+            boolean pass = pinCheck.check();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
